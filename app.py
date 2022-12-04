6dfa33902712d4abf45ec868c05b029f99dc0bb8
WardJaeger <wardjaeger@gmail.com>
2022-12-03 21:21:10 -0600

    Update streamlit, add Heroku runtime and requirements

--- app.py
+++ app.py
@@ -1,47 +1,11 @@
-import pandas as pd 
 import joblib
+import matplotlib.pyplot as plt
+import pandas as pd 
 import streamlit as st
 
 
 # Load model pipeline 
 model = joblib.load(open("model-tpot.joblib","rb"))
-
-def visualize_confidence_level(prediction_proba):
-    """
-    this function uses matplotlib to create inference bar chart rendered with streamlit in real-time 
-    return type : matplotlib bar chart  
-    """
-    data = (prediction_proba[0]*100).round(2)
-    grad_percentage = pd.DataFrame(data = data, columns = ['Percentage'], index = ['Dairy', 'Fruit', 'Grain', 'Protein', 'Sweets', 'Vegetable'])
-    ax = grad_percentage.plot(kind='barh', figsize=(7, 4), color='#722f37', zorder=10, width=0.5)
-    ax.legend().set_visible(False)
-    ax.set_xlim(xmin=0, xmax=100)
-    
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.spines['left'].set_visible(True)
-    ax.spines['bottom'].set_visible(True)
-
-    ax.tick_params(axis="both", which="both", bottom="off", top="off", labelbottom="on", left="off", right="off", labelleft="on")
-    
-    vals = ax.get_xticks()
-    for tick in vals:
-        ax.axvline(x=tick, linestyle='dashed', alpha=0.4, color='#eeeeee', zorder=1)
-
-    ax.set_xlabel("Percentage(%) Confidence Level", labelpad=2, weight='bold', size=12)
-    ax.set_ylabel("Food Group", labelpad=10, weight='bold', size=12)
-    ax.set_title('Prediction Confidence Level', fontdict=None, loc='center', pad=None, weight='bold')
-
-    st.pyplot()
-    return
-
-st.write("""
-# Wine Quality Prediction ML Web-App 
-This app predicts the ** Quality of Wine **  using **wine features** input via the **side panel** 
-""")
-
-st.sidebar.header('User Input Parameters') #user input parameter collection with streamlit side bar
-st.sidebar.write('Please provide all the following nutritional values per serving.')
 
 def get_user_input():
     """
@@ -79,12 +43,50 @@ def get_user_input():
 
     return data
 
+
+def visualize_confidence_level(prediction_proba):
+    """
+    this function uses matplotlib to create inference bar chart rendered with streamlit in real-time 
+    return type : matplotlib bar chart  
+    """
+    ax = prediction_proba.plot(kind='barh', figsize=(7, 4), color='#722f37', zorder=10, width=0.5)
+    ax.legend().set_visible(False)
+    ax.set_xlim(xmin=0, xmax=100)
+    
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['left'].set_visible(True)
+    ax.spines['bottom'].set_visible(True)
+
+    ax.tick_params(axis="both", which="both", bottom="off", top="off", labelbottom="on", left="off", right="off", labelleft="on")
+    
+    vals = ax.get_xticks()
+    for tick in vals:
+        ax.axvline(x=tick, linestyle='dashed', alpha=0.4, color='#eeeeee', zorder=1)
+
+    ax.set_xlabel("Percentage(%) Confidence Level", labelpad=2, weight='bold', size=12)
+    ax.set_ylabel("Food Group", labelpad=10, weight='bold', size=12)
+    ax.set_title('Prediction Confidence Level', fontdict=None, loc='center', pad=None, weight='bold')
+
+    st.pyplot(ax.get_figure())
+
+
+st.header('Food Group Classification')
+st.write('This application predicts the food groups of various food items, based on nutritional data provided by the user.')
+
+st.sidebar.header('User Input Parameters')
+st.sidebar.write('Please provide all the following nutritional values per serving.')
 user_input = get_user_input()
 
 st.subheader('User Input parameters')
+st.write('The predictive model accepts features that have been normalized for a 100 gram sample. The data shown below has been adjusted accordingly, and may differ from what the user inputs into the sidebar.')
 st.write(user_input)
 
-prediction = model.predict(user_input)
-prediction_proba = model.predict_proba(user_input)
-
+st.subheader('Prediction')
+prediction = model.predict(user_input)[0]
+prediction_proba = pd.DataFrame(
+    data = (model.predict_proba(user_input)[0]*100).round(2), 
+    index = ['Dairy', 'Fruits', 'Grains', 'Protein', 'Sweets', 'Vegetables']
+)
+st.write(f"The model classifies this food into the food group of {prediction}, with a confidence level of {prediction_proba.loc[prediction][0]}%.")
 visualize_confidence_level(prediction_proba)
